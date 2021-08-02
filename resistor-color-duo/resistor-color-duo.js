e795c8d642b48acd8c6b64365e62fda086b17bc8
truongdq.dev <truongdq.dev@gmail.com>
2021-08-02 20:20:41 +0700

    learn react day 1

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (arrCode) => {
+  return Number(`${COLORS.indexOf(arrCode[0])}${COLORS.indexOf(arrCode[1])}`)
+};
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
