4388ca64ed4e169488cb9a97cd43396be26f6fe3
truongdq.dev <truongdq.dev@gmail.com>
2021-08-04 13:31:46 +0700

    fix full test

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (time = new Date()) => {
+  return new Date(time.getTime() + Math.pow(10, 12))
 };
